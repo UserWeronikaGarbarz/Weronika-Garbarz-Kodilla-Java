dcc3c5e5f0d5534a4d8525d3f13d859c68e6e87c
UserWeronikaGarbarz <weronika.garbarz@gmail.com>
2019-12-10 15:18:44 +0100

    First commit

--- src/main/java/com/kodilla/FirstClass.java
+++ src/main/java/com/kodilla/FirstClass.java
@@ -1,4 +1,21 @@
 package com.kodilla;
 
-public class FirstClass {
+import java.lang.*;
+
+
+class Calculator {
+
+    public void addAToB() {
+        System.out.println("It works!");
+    }
+
+    public void subtractAFromB() {
+
+    }
+
+    public static void main(String args[]) {
+        com.kodilla.Calculator calculator = new com.kodilla.Calculator();
+        calculator.addAToB();
+        calculator.subtractAFromB();
+    }
 }
