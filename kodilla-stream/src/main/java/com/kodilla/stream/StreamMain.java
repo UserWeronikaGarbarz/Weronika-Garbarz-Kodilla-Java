12647bac185663bca80110e41d4615ba23140ee7
UserWeronikaGarbarz <weronika.garbarz@gmail.com>
2020-01-21 21:04:24 +0100

    Task 7.1 - Lambda

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,22 +1,40 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.lambda.ExecuteSaySomething;
-import com.kodilla.stream.lambda.Executor;
-import com.kodilla.stream.lambda.Processor;
-import com.kodilla.stream.lambda.SaySomething;
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.*;
+import com.kodilla.stream.reference.FunctionalCalculator;
+import java.lang.String;
 
 public class StreamMain {
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to module 7 - Stream");
-
         SaySomething saySomething = new SaySomething();
         saySomething.say();
 
         //object - class implements interface Executor; method execute
         Processor processor = new Processor();
-
         processor.execute (() -> System.out.println("This is an example text"));
+
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+        System.out.println("Calculating expressions with lambdas");
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        System.out.println("Text Beautifier:");
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.beautify("Kodilla", (text) -> text.toUpperCase());
+        poemBeautifier.beautify("Kodilla", (text) -> "ABC" + text + "ABC");
+        poemBeautifier.beautify("KODILLA", (text) -> text.toLowerCase());
+        poemBeautifier.beautify("KODILLA", (text) -> text.replaceFirst("ILLA", "KOD"));
     }
 }
