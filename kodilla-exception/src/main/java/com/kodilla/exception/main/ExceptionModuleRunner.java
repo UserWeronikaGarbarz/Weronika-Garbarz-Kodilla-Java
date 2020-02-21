830dd601fd44d16f9a1b72b6d0789e0a6ad3c03d
UserWeronikaGarbarz <weronika.garbarz@gmail.com>
2020-02-21 13:42:51 +0100

    Task 8.3 - Throw/Throws

--- kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -1,18 +1,18 @@
 package com.kodilla.exception.main;
 
-import com.kodilla.exception.test.FirstChallenge;
+import com.kodilla.exception.test.ExceptionHandling;
+import com.kodilla.exception.test.SecondChallenge;
 
 public class ExceptionModuleRunner {
 
     public static void main(String[] args) {
 
-        try {
-            FirstChallenge firstChallenge = new FirstChallenge();
-            double result = firstChallenge.divide(3, 0);
-            System.out.println(result);
+        SecondChallenge secondChallenge = new SecondChallenge();
 
-        } catch (ArithmeticException e) {
-            System.out.println("Not dividing by zero!");
+        try {
+            secondChallenge.probablyIWillThrowException(2.0, 1.5);
+        } catch (ExceptionHandling e) {
+            System.out.println("Exceptiotn handled.");
         }
     }
 }
