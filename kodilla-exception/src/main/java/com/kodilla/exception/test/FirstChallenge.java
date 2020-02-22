90fa65ce5909449cdae32f460eb97e1c456efe63
UserWeronikaGarbarz <weronika.garbarz@gmail.com>
2020-02-22 02:42:54 +0100

    Task 8.3

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -9,3 +9,18 @@ public class FirstChallenge {
         return a / b;
     }
 }
+
+class FirstChallengeRunner {
+
+    public static void main(String[] args) {
+
+        FirstChallenge firstChallenge = new FirstChallenge();
+
+        try {
+            double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println("Not dividing by zero!");
+        }
+    }
+}
