28a8e069606a17aa35421201f100dae613f54d11
UserWeronikaGarbarz <weronika.garbarz@gmail.com>
2020-03-06 11:56:19 +0100

    Task 11.5

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -0,0 +1,25 @@
+package com.kodilla.patterns.builder.bigmac;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BigmacTestSuite {
+    @Test
+    public void testBigmac() {
+        //Given
+        Bigmac bigmac = new Bigmac.BigmacBuilder()
+                .bun("Sezam")
+                .burgers(3)
+                .sauce("1000 wysp")
+                .ingredient("Bekon")
+                .ingredient("Shrimps")
+                .ingredient("Salad")
+                .ingredient("Onion")
+                .build();
+        System.out.println(bigmac);
+        //When
+        int howManyIngredients = bigmac.getIngredients().size();
+        //Then
+        Assert.assertEquals(4, howManyIngredients);
+    }
+}
