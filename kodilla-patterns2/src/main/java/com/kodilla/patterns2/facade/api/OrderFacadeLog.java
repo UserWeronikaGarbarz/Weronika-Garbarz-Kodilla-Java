9d7c5383e729d4c8205d8684b5daa878f1e1fb60
UserWeronikaGarbarz <weronika.garbarz@gmail.com>
2020-04-09 15:40:26 +0200

    Commit

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacadeLog.java
@@ -0,0 +1,27 @@
+package com.kodilla.patterns2.facade.api;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class OrderFacadeLog {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderFacade.class);
+
+
+    private int counter = 0;
+
+    @Pointcut("execution( * com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..) )")
+    public void methodsToCount() {
+    }
+
+    @Before("methodsToCount()")
+    public void execute() {
+        counter++;
+        LOGGER.info("method invocation: " + counter);
+    }
+}
